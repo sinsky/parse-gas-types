808097717b29b5a2d66b424e8a10110b022cdb28
sinsky <60662802+sinsky@users.noreply.github.com>
2024-10-03 00:20:16 +0900

    chore: prettier formated

--- scripts/parse-txt2types.ts
+++ scripts/parse-txt2types.ts
@@ -2,30 +2,30 @@ import { escapeHTML } from "bun";
 import fs from "fs";
 import path from "path";
 
-const [,, inputFile] = process.argv;
+const [, , inputFile] = process.argv;
 
 if (!inputFile) {
-  console.error('Usage: bun run script.ts <inputFile>');
+  console.error("Usage: bun run script.ts <inputFile>");
   process.exit(1);
 }
 
 const basePath = path.join(__dirname, "..");
 const inputFilePath = path.join(basePath, inputFile);
-const rawFileData = fs.readFileSync(inputFilePath, 'utf8');
+const rawFileData = fs.readFileSync(inputFilePath, "utf8");
 
 const unescapedContent = rawFileData
-    .replace(/\\\\/g, "\\")
-    .replace(/\\"/g, '"');
+  .replace(/\\\\/g, "\\")
+  .replace(/\\"/g, '"');
 
 const jsonData: string[][] = JSON.parse(unescapedContent);
 
 function unescapeUnicode(str: string) {
-    return str.replace(/\\u[\dA-Fa-f]{4}/g, match => {
-        return String.fromCharCode(parseInt(match.replace('\\u', ''), 16));
-    });
+  return str.replace(/\\u[\dA-Fa-f]{4}/g, (match) => {
+    return String.fromCharCode(parseInt(match.replace("\\u", ""), 16));
+  });
 }
 
-const fileData = jsonData[0].map(line => {
+const fileData = jsonData[0].map((line) => {
   const [filename, data] = line;
   const unescapedContent = data.replace(/\\n/g, "\n");
   const finalContent = unescapeUnicode(unescapedContent);
@@ -43,4 +43,4 @@ fileData.forEach(([filename, data]) => {
   fs.writeFileSync(filePath, data, "utf8");
 });
 
-console.log('Files have been created successfully.');
+console.log("Files have been created successfully.");
